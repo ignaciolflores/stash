0cae66d4e646a4228b2744f4aa6d1e8df16ff76c
Ignacio L Flores <ignacio@introbay.com>
2015-02-25 01:01:55 +0100

    Change permisions of stash home on run to avoid problems when mapping stash-home per container.

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@
 
 # Start Atlassian in the forground
 sysctl -w kernel.shmmax=714219520
+chown -R $STASHUSR $STASH_HOME
 su - "$STASHUSR" -c "export STASH_HOME=$STASH_HOME ; /opt/atlassian/atlassian-stash-$AppVer/bin/start-stash.sh -fg"
