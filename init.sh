cb0ad6f20924bb6f1982c352e8477c25862e5976
Ignacio López Flores <ignacio@introbay.com>
2015-09-23 09:24:18 +0200

    Changed appname with variable

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@
 # Start Atlassian in the forground
 sysctl -w kernel.shmmax=714219520
 chown -R $STASHUSR $STASH_HOME
-su - "$STASHUSR" -c "export STASH_HOME=$STASH_HOME ; /opt/atlassian/atlassian-bitbucket-$AppVer/bin/start-stash.sh -fg"
+su - "$STASHUSR" -c "export STASH_HOME=$STASH_HOME ; /opt/atlassian/atlassian-$AppName-$AppVer/bin/start-$AppName.sh -fg"
