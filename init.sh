09382e41e34422744231707a7026053291cf7c28
Ignacio López Flores <ignacio@introbay.com>
2015-09-23 10:06:04 +0200

    Updated to bitbucket

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@
 
 # Start Atlassian in the forground
 sysctl -w kernel.shmmax=714219520
-chown -R $STASHUSR $STASH_HOME
-su - "$STASHUSR" -c "export STASH_HOME=$STASH_HOME ; /opt/atlassian/atlassian-$AppName-$AppVer/bin/start-$AppName.sh -fg"
+chown -R $BITBUCKETUSR $BITBUCKET_HOME
+su - "$BITBUCKETUSR" -c "export BITBUCKET_HOME=$BITBUCKET_HOME ; /opt/atlassian/atlassian-$AppName-$AppVer/bin/start-$AppName.sh -fg"
